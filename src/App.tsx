fbc6ca6422f0a838477b28c5d8387f6f513b29b8
Medina <mkazic1@etf.unsa.ba>
2022-12-02 01:25:26 +0100

    Added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import Movies from './components/Movies';
+import NotFoundPage from './components/NotFoundPage';
+import Overview from './components/Overview';
+import TvShows from './components/TvShows';
 
-function App () {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Overview/>}/>
+        <Route path="/movies" element={<Movies/>}/>
+        <Route path="/tvshows" element={<TvShows/>}/>
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
