d2140984e6fa94634e69508dd08e0c594acb109c
Medina <mkazic1@etf.unsa.ba>
2022-12-02 17:33:02 +0100

    Replaced file location

--- src/components/NotFoundPage.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const NotFoundPage = () => {
-  return <h1>This page could not be found!</h1>
-}
-
-export default NotFoundPage;
--- src/components/TvShows.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const TvShows = () => {
-  return <h1>TvShows</h1>
-}
-
-export default TvShows;
