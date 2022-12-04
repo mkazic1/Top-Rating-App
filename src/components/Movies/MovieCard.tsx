b9bba71786d631b3ba51c88e7bc009120fae9c6b
Medina <mkazic1@etf.unsa.ba>
2022-12-04 01:37:32 +0100

    Implemented displaying movies logic

---
+++ src/components/Movies/MovieCard.tsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import MoviesInterface from './MoviesInterface';
+import styles from '../CommonStyle.module.css';
+
+const API_IMG = 'https://image.tmdb.org/t/p/w500/'
+
+const MovieCard = (props: MoviesInterface) => {
+  return (
+    <div className={styles.card}>
+      <img className={styles.image} src={API_IMG + props.poster_path} />
+      <div className={styles.body}>
+        <h3 className={styles.title}>{props.title}</h3>
+        <button className={styles.button}>Details</button>
+      </div>
+    </div>
+  );
+}
+
+export default MovieCard;
