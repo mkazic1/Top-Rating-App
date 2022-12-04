aa74170744eb7738067cdf5b9278abd308cafcfe
Medina <mkazic1@etf.unsa.ba>
2022-12-04 01:37:50 +0100

    Implemented displaying tv shows logic

--- src/components/TvShows/TvShows.tsx
+++ src/components/TvShows/TvShows.tsx
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import TvShowCard from './TvShowCard';
+import TvShowsInterface from './TvShowsInterface';
+import styles from '../CommonStyle.module.css';
 
-const TvShows = () => {
-  return <h1>TvShows</h1>
+const API_KEY = process.env.REACT_APP_API;
+const API_TV_URL = 'https://api.themoviedb.org/3/tv/top_rated?api_key=' + API_KEY;
+
+const TvShows: React.FC = () => {
+  const [tvShows, setTvShows] = useState([]);
+
+  useEffect(() => {
+    fetch(API_TV_URL)
+      .then(async (data) => await data.json())
+      .then(data => {
+        setTvShows(data.results);
+      }
+      )
+  }, []);
+
+  const topTenTvShows: TvShowsInterface[] = tvShows.slice(0, 10);
+
+  return (
+    <div className={styles['grid-container']}>
+      <div className={styles.grid}>
+        {topTenTvShows?.map((tvShow) => <TvShowCard key={tvShow?.id} {...tvShow}/>)}
+      </div>
+    </div>
+  );
 }
 
 export default TvShows;
---
+++ src/components/TvShows/TvShowsInterface.ts
@@ -0,0 +1,8 @@
+export default interface TvShowInterface {
+  id: number
+  name: string
+  poster_path: string
+  release_date: string
+  overview: string
+  vote_average: number
+}
