3021a16c622482588861d6015667ba433e1cfcf4
Medina <mkazic1@etf.unsa.ba>
2022-12-05 22:08:50 +0100

    Tv show details implemented - not finished

--- src/components/TvShows/TvShowCard.tsx
+++ src/components/TvShows/TvShowCard.tsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import TvShowsInterface from '../../interfaces/TvShowsInterface';
 import styles from '../../style/CommonStyle.module.css';
 import { API_IMG } from '../../constants/url';
+import { TVSHOWS } from '../../constants/routes';
 
 const TvShowCard = (props: TvShowsInterface) => {
   return (
@@ -9,9 +11,11 @@ const TvShowCard = (props: TvShowsInterface) => {
       <img className={styles.image} src={API_IMG + props.poster_path} />
       <div className={styles.body}>
         <h3 className={styles.title}>{props.name}</h3>
-        <button className={styles['card-button']}>
+        <Link to={`${TVSHOWS}/${props.id}`}>
+          <button className={styles['card-button']}>
             Details
-        </button>
+          </button>
+        </Link>
       </div>
     </div>
   );
---
+++ src/components/TvShows/TvShowDetails.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from '../../style/CommonStyle.module.css';
+
+const TvShowDetails = () => {
+  return (
+    <div className={styles.container}>
+      <h2>Tv show details not finished</h2>
+    </div>
+  )
+}
+
+export default TvShowDetails;
