3039ca16ebe024995df102b82cb60f8b89974194
Medina <mkazic1@etf.unsa.ba>
2022-12-05 22:08:32 +0100

    Movie details implemented - not finished

--- src/components/Movies/MovieCard.tsx
+++ src/components/Movies/MovieCard.tsx
@@ -1,19 +1,25 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import MoviesInterface from '../../interfaces/MoviesInterface';
 import styles from '../../style/CommonStyle.module.css';
 import { API_IMG } from '../../constants/url';
+import { MOVIES } from '../../constants/routes';
 
 const MovieCard = (props: MoviesInterface) => {
   return (
-    <div className={styles.card}>
-      <img className={styles.image} src={API_IMG + props.poster_path} />
-      <div className={styles.body}>
-        <h3 className={styles.title}>{props.title}</h3>
-        <button className={styles['card-button']}>
+    <>
+      <div className={styles.card}>
+        <img className={styles.image} src={API_IMG + props.poster_path} />
+        <div className={styles.body}>
+          <h3 className={styles.title}>{props.title}</h3>
+          <Link to={`${MOVIES}/${props.id}`}>
+            <button className={styles['card-button']}>
             Details
-        </button>
+            </button>
+          </Link>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
